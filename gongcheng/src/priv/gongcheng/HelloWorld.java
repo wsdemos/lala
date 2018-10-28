94bbfbe448933271c14e8799534a724346c9dfe7
wsdemos <179575262@qq.com>
2018-10-28 15:28:24 +0800

    a

---
+++ gongcheng/src/priv/gongcheng/HelloWorld.java
@@ -0,0 +1,15 @@
+package priv.gongcheng;
+
+public class HelloWorld {
+	private static String say="不是让你怀念前任\r\n" + 
+			"是告诉你要珍惜眼前人\r\n" + 
+			"不要等失去了才知道后悔\r\n" + 
+			"不要让爱你的现任变成前任\r\n" + 
+			"不负遇见";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("前任3"+say);
+
+	}
+
+}

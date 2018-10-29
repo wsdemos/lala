b2df824c55de32c7b753cc6518774bc65433577e
wsdemos <179575262@qq.com>
2018-10-29 17:42:25 +0800

    a

--- gongcheng/src/priv/gongcheng/HelloWorld.java
+++ gongcheng/src/priv/gongcheng/HelloWorld.java
@@ -6,8 +6,6 @@ public class HelloWorld {
 			"��Ҫ��ʧȥ�˲�֪�����\r\n" + 
 			"��Ҫ�ð�������α��ǰ��\r\n" + 
 			"��������";
-	public void post() {
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ǰ��3"+say);

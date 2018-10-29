7954072248e6d01d7ea51811bc18503c1f3b737a
wsdemos <179575262@qq.com>
2018-10-29 17:49:04 +0800

    a

--- gongcheng/src/priv/gongcheng/HelloWorld.java
+++ gongcheng/src/priv/gongcheng/HelloWorld.java
@@ -6,12 +6,6 @@ public class HelloWorld {
 			"��Ҫ��ʧȥ�˲�֪�����\r\n" + 
 			"��Ҫ�ð�������α��ǰ��\r\n" + 
 			"��������";
-<<<<<<< HEAD
-=======
-	public void ll() {
-		System.out.println("lala");
-	}
->>>>>>> refs/heads/branch1
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ǰ��3"+say);
